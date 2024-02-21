2cb68e65a78657240afc1ad8764b5309d44b0a2e
AlvaroRJ01 <alvaro.romero.jimenez@ces-vegamedia.es>
2024-02-21 23:18:09 +0100

    Agregar archivos de proyecto.

---
+++ VegaJuego/PantallaInicio.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace VegaJuego
+{
+    partial class PantallaInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PantallaInicio));
+            button1 = new Button();
+            checkSonido = new CheckBox();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.BackgroundImage = Properties.Resources.Boton;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(327, 402);
+            button1.Name = "button1";
+            button1.Size = new Size(282, 45);
+            button1.TabIndex = 0;
+            button1.Text = "Comienza la aventura";
+            button1.TextAlign = ContentAlignment.MiddleRight;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // checkSonido
+            // 
+            checkSonido.AutoSize = true;
+            checkSonido.BackColor = Color.Transparent;
+            checkSonido.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            checkSonido.ForeColor = Color.Black;
+            checkSonido.Location = new Point(508, 195);
+            checkSonido.Name = "checkSonido";
+            checkSonido.Size = new Size(151, 27);
+            checkSonido.TabIndex = 1;
+            checkSonido.Text = "Sonido On/Off";
+            checkSonido.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(470, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(301, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Creado por Andrea Romero Jimenez";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.ZeldaLogo;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(508, 90);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(214, 87);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = Properties.Resources.LogoBueno;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox2.Location = new Point(-15, -1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(325, 233);
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // PantallaInicio
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackgroundImage = Properties.Resources.FondoPantalla;
+            ClientSize = new Size(804, 459);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(checkSonido);
+            Controls.Add(button1);
+            ForeColor = Color.Gold;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "PantallaInicio";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PantallaInicio";
+            FormClosed += PantallaInicio_FormClosed;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private CheckBox checkSonido;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
